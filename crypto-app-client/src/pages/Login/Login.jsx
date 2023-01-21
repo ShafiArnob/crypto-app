f84b626e4783e10c9d8c908c8346736661d8cbc8
ShafiArnob <shafiarnob@gmail.com>
2023-01-21 20:38:18 +0600

    login to firebase

--- crypto-app-client/src/pages/Login/Login.jsx
+++ crypto-app-client/src/pages/Login/Login.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 import './Login.css'
+import { projectAuth } from '../../firebase/config';
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [ signInWithEmailAndPassword, user, loading, error,] = useSignInWithEmailAndPassword(projectAuth);
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    await signInWithEmailAndPassword(email,password)
+  }
+  // console.log(user);
   return (
     <div className='login-page'>
       <div className='login-form'>
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <p>Email: </p>
-            <input type="email" name="email" id="email" />
+            <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" name="email" id="email" />
           </div>
 
           <div>
             <p>Password: </p>
-            <input type="password" name="password" id="password" />
+            <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" name="password" id="password" />
           </div>
           <button>Login</button>
         </form>
