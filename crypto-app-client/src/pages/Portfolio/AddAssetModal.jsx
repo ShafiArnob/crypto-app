91fca306ecf6f6635d979d5e37cdf313ccfb2396
ShafiArnob <shafiarnob@gmail.com>
2023-01-23 05:37:31 +0600

    add asset modal select coin dropdown

--- crypto-app-client/src/pages/Portfolio/AddAssetModal.jsx
+++ crypto-app-client/src/pages/Portfolio/AddAssetModal.jsx
@@ -1,7 +1,11 @@
 import Modal from 'react-modal'
 import './Portfolio.css'
 import {IoMdClose} from 'react-icons/io'
+import { mockMarketList } from '../../utils/constants/mock'
+import { useState } from 'react'
 const AddAssetModal = ({openModal, setopenModal}) => {
+  const [selecCoin, setSelecCoin] = useState('')
+  console.log(selecCoin);
   return (
     <Modal className="Modal" overlayClassName="Overlay" isOpen={openModal} onRequestClose ={()=>setopenModal(!openModal)} ariaHideApp={false}>
       {/* Modal Header */}
@@ -11,7 +15,19 @@ const AddAssetModal = ({openModal, setopenModal}) => {
       </div>
       {/*Modal Body */}
       <div className='modal-body'>
-        
+        <div className='coin-dropdown'>
+          {/* <label htmlFor="coins">Select Coin</label> */}
+          <select onChange={(e)=>setSelecCoin(e.target.value)} name="coins" id="coins">
+            <option value="">Select Coin</option>
+            {
+              mockMarketList.map(coin=>(
+                <option key={coin.id} value={coin.id}>
+                    {coin.name}
+                </option>
+              ))
+            }
+          </select>
+        </div>
       </div>
     </Modal>
   )
