c51e4f41b4e449817e8276f49c6f0a04c6b7239d
ShafiArnob <shafiarnob@gmail.com>
2023-01-23 11:17:37 +0600

    modal total spent

--- crypto-app-client/src/pages/Portfolio/AddAssetModal.jsx
+++ crypto-app-client/src/pages/Portfolio/AddAssetModal.jsx
@@ -3,13 +3,15 @@ import './Portfolio.css'
 import {IoMdClose} from 'react-icons/io'
 import { mockMarketList } from '../../utils/constants/mock'
 import { useState } from 'react'
+import DatePicker from 'react-date-picker'
 const AddAssetModal = ({openModal, setopenModal}) => {
   const [transactionType,setTransactionType] = useState('buy')
   const [selecCoin, setSelecCoin] = useState('')
   const [quantity, setQuantity] = useState(0)
   const [pricePerCoin, setPricePerCoin] = useState(0)
-
-  // console.log(transactionType);
+  const [date, setDate] = useState(new Date())
+ 
+  // console.log(date);
   return (
     <Modal className="Modal" overlayClassName="Overlay" isOpen={openModal} onRequestClose ={()=>setopenModal(!openModal)} ariaHideApp={false}>
       {/* Modal Header */}
@@ -39,12 +41,21 @@ const AddAssetModal = ({openModal, setopenModal}) => {
         <div className='coin-values'>
           <div className='coin-value-cell'>
             <p>Quantity</p>
-            <input onChange={(e)=>setQuantity(e.target.value)} value={quantity}type="number" id='quantity'/>
+            <input onChange={(e)=>setQuantity(e.target.value)} value={quantity} min="0" type="number" id='quantity'/>
           </div>
           <div className='coin-value-cell'>
             <p>Price Per Coin</p>
-            <input onChange={(e)=>setPricePerCoin(e.target.value)} value={pricePerCoin} type="number" id='price-per-coin'/>
+            <input onChange={(e)=>setPricePerCoin(e.target.value)} value={pricePerCoin} min="0" type="number" id='price-per-coin'/>
           </div>
+        </div>
+
+        <div className='date'>
+          <DatePicker className="date-picker" onChange={setDate} value={date} format="dd/MM/y"/>
+        </div>
+
+        <div className='total-spent'>
+          <p>Total Spent</p>
+          <h1>${quantity*pricePerCoin}</h1>
         </div>
       </div>
     </Modal>
