a6f48661456d72927f4507b44ca95aef04e0046b
ShafiArnob <shafiarnob@gmail.com>
2023-01-26 04:47:37 +0600

    fetch individual market data

--- crypto-app-client/src/pages/Crypto/Crypto.jsx
+++ crypto-app-client/src/pages/Crypto/Crypto.jsx
@@ -3,54 +3,26 @@ import { marketData, mockCoinData } from '../../utils/constants/mock'
 import {AiFillCaretDown, AiFillCaretUp} from 'react-icons/ai'
 import { Area, AreaChart, CartesianGrid, ReferenceLine, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { convertUnixTimestampToDate } from '../../utils/helpers/date-helper'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
 const Crypto = () => {
   const [coinData, setCoinData] = useState(mockCoinData)
   const [chartData, setChartData] = useState(marketData)
-  console.log(coinData);
-  const data = [
-    {
-      "name": "Page A",
-      "uv": 4000,
-      "pv": 2400,
-      "amt": 2400
-    },
-    {
-      "name": "Page B",
-      "uv": 3000,
-      "pv": 1398,
-      "amt": 2210
-    },
-    {
-      "name": "Page C",
-      "uv": 2000,
-      "pv": 9800,
-      "amt": 2290
-    },
-    {
-      "name": "Page D",
-      "uv": 2780,
-      "pv": 3908,
-      "amt": 2000
-    },
-    {
-      "name": "Page E",
-      "uv": 1890,
-      "pv": 4800,
-      "amt": 2181
-    },
-    {
-      "name": "Page F",
-      "uv": 2390,
-      "pv": 3800,
-      "amt": 2500
-    },
-    {
-      "name": "Page G",
-      "uv": 3490,
-      "pv": 4300,
-      "amt": 2100
+  const {id} = useParams()
+  // console.log(id);
+  
+  useEffect(()=>{
+    const getCoinData = async() => {
+      await axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`)
+      .then(data => setCoinData(data.data))
+      .catch(error => {
+        console.log(error)
+      })
     }
-  ]
+    
+    getCoinData()
+  },[])
   const formatPriceData = (data) => {
     return data.prices.map(price=>{
       return {
@@ -59,9 +31,12 @@ const Crypto = () => {
       }
     })
   }
-
   const priceData = formatPriceData(chartData)
-  console.log(priceData);
+  
+  // if(coinData){
+  //   console.log(5);
+  //   return <p>Loading...</p>
+  // }
   return (
     <div className="crypto-info">
       {/* Price Info */}
